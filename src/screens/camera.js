37f22ba261f3583693534f5a037f724aa9f1e2de
unknown <denniszax32@gmail.com>
2020-11-14 13:06:16 -0800

    Camera

--- src/screens/camera.js
+++ src/screens/camera.js
@@ -1,11 +1,156 @@
-import React from 'react'
-import {View,Text} from 'react-native'
+import React,{useState,useEffect} from 'react'
+import {
+    SafeAreaView,
+    Text,
+    TouchableOpacity,
+    Modal,
+    StyleSheet,
+    Button,
+    Image,
+    Dimensions,
+    ScrollView,
+    RefreshControl,
+    View,
+    ActivityIndicator
+  } from 'react-native'
+import { Camera } from 'expo-camera';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import upload from '../api/upload';
+// nutricam
+export default ({navigation})=>{
+    const [hasPermission, setHasPermission] = useState(null);
+    const [cameraRef,setCameraRef]=useState(null);
+    const [type, setType] = useState(Camera.Constants.Type.back);
+    const [isLoading,setLoading] = useState(false);
+    const [error,setError]=useState(null);
+    useEffect(() => {
+      (async () => {
+        const { status } = await Camera.requestPermissionsAsync();
+        setHasPermission(status === 'granted');
+      })();
+    }, []);
+    if (hasPermission === null) {
+      return null;
+    }
+    if (hasPermission === false) {
+      return <Text>For the app to work camera permissions are required</Text>;
+    }
+  
+    apiReq=async (base64)=>{
+        
+     setError(null);
+      var params = new URLSearchParams();
+      params.append('image',base64);
+      upload.post("/uploadImage",params)
+      .then(async (response)=>{
+        //console.log(response.data);
+        setLoading(false);
+        if(response.data["result"]!=undefined){
+            const res=await AsyncStorage.getItem('foods');
+            console.log(res);
+            res2=JSON.parse(res);
+            if(!res2){
+                res2=[]
+            }
+            res2.push(response.data["result"]);
+            await AsyncStorage.setItem('foods',JSON.stringify(res2));
+            setLoading(false);
+            navigation.navigate("home",{newEntry:true});
+          //setData({type:response.data["result"]["type"],fruit:response.data["result"]["fruit"]})
+        }else{
+          if(response.data["error"]!=undefined){
+            setError(response.data["error"]);
+          }else{
+            setError("An unknown error happened on the server");
+          }
+          //setData("There was a server error")
+        }
+        
+       // return response.data;
+      })
+      .catch((error)=>{
+        console.log(error);
+        setError(error);
+        setLoading(false);
+        // return "error";
+        
+      })
+      
 
+    // setLoading(false);
+      
+    }
+    if(isLoading){
+      return (
+        <SafeAreaView style={styles.container}>
+            <ActivityIndicator size={"large"} />
+            <Text>Please wait...</Text>
+          </SafeAreaView>
+      );
+    }
 
-export default ()=>{
     return (
-        <>
-            <Text>Camera</Text>
-        </>
+      <View style={{ flex: 1 }}>
+        <Camera style={{ flex: 1 }} type={type} ref={ref => {
+          setCameraRef(ref) ;
+    }}>
+          <View
+            style={{
+              flex: 1,
+              backgroundColor: 'transparent',
+              justifyContent: 'flex-end'
+            }}>
+            <TouchableOpacity
+              style={{
+                flex: 0.1,
+                alignSelf: 'flex-end'
+              }}
+              onPress={() => {
+                setType(
+                  type === Camera.Constants.Type.back
+                    ? Camera.Constants.Type.front
+                    : Camera.Constants.Type.back
+                );
+              }}>
+              <Text style={{ fontSize: 18, marginBottom: 10, color: 'white' }}> Flip </Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={{alignSelf: 'center'}} onPress={async() => {
+              if(cameraRef){
+                const {base64} = await cameraRef.takePictureAsync(options={base64:true,quality:0});              
+                 setLoading(true);
+                 await apiReq(base64);
+              }
+            }}>
+              <View style={{ 
+                 borderWidth: 2,
+                 borderRadius:"50%",
+                 borderColor: 'white',
+                 height: 50,
+                 width:50,
+                 display: 'flex',
+                 justifyContent: 'center',
+                 alignItems: 'center'}}
+              >
+                <View style={{
+                   borderWidth: 2,
+                   borderRadius:"50%",
+                   borderColor: 'white',
+                   height: 40,
+                   width:40,
+                   backgroundColor: 'white'}} >
+                </View>
+              </View>
+            </TouchableOpacity>
+          </View>
+        </Camera>
+      </View>
     );
-}
+  }
+  
+  const styles=StyleSheet.create({
+      container:{
+        flex:1,
+        justifyContent:"center",
+        alignItems:"center"
+      }
+  });
--- src/screens/welcome.js
+++ src/screens/welcome.js
@@ -5,8 +5,7 @@ import {View,Text,Button} from 'react-native'
 export default ({navigation})=>{
     return (
         <>
-            <Text>Welcome</Text>
-            <Button onPress={()=>navigation.navigate('mainFlow')} title={"Go to main flow"}/>
+            <Button onPress={()=>navigation.navigate("mainFlow")} title={"Go to main flow"}/>
         </>
     );
 }
